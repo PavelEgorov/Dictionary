17a45d31890f2de2d92336ca7c240a010e5bb6ce
PavelEgorov <jonnycomein@gmail.com>
2020-06-17 18:24:53 +0300

    Coroutines

--- app/src/main/java/com/egorovfond/dictionary/usecases/DictionaryModel.kt
+++ app/src/main/java/com/egorovfond/dictionary/usecases/DictionaryModel.kt
@@ -1,7 +1,9 @@
 package com.egorovfond.dictionary.usecases
 
 import com.egorovfond.dictionary.entities.IDatabase
+import com.egorovfond.dictionary.entities.data.SearchResult
 import io.reactivex.rxjava3.schedulers.Schedulers
+import javax.inject.Inject
 
 class DictionaryModel @Inject constructor(val database : IDatabase): IDictionary {
     override suspend fun getByText(text: String): List<SearchResult> {
