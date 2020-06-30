bb2b7ac2f161c533375643c9ceccf8bb942b51c0
PavelEgorov <jonnycomein@gmail.com>
2020-06-30 19:50:53 +0300

    Android 10

--- app/src/main/java/com/egorovfond/dictionary/ui/MainActivity.kt
+++ app/src/main/java/com/egorovfond/dictionary/ui/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.egorovfond.dictionary.ui
 
 import android.content.Intent
+import android.content.res.Configuration
+import android.os.Build
 import android.os.Bundle
+import android.provider.Settings
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.LinearLayout
 import android.widget.Toast
 import androidx.lifecycle.Observer
@@ -160,6 +165,7 @@ class MainActivity : BaseActivity<List<SearchResult>, MainInteractor>() {
             }
         }
     }
+
     override fun onResume() {
         super.onResume()
         appUpdateManager
@@ -179,6 +185,25 @@ class MainActivity : BaseActivity<List<SearchResult>, MainInteractor>() {
                     )
                 }
             }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+       menu?.findItem(R.id.menu_screen_settings)?.isVisible =
+            Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+                R.id.menu_screen_settings -> {
+                    startActivityForResult(
+                        Intent(Settings.Panel.ACTION_INTERNET_CONNECTIVITY),
+                        42)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 
 }
